027f1cb00c8117e2740584ec4932e671a5f957df
Priyanka Gupta <priyankagjune@gmail.com>
2024-02-25 18:34:13 -0700

    usecontext

---
+++ src/ComponentC.jsx
@@ -0,0 +1,12 @@
+import ComponentD from "./ComponentD"
+
+function ComponentC(){
+    return (  
+    <div className="box">
+    <h1>ComponentC</h1>
+    <h2></h2>
+    <ComponentD />
+</div>)
+}
+
+export default ComponentC
